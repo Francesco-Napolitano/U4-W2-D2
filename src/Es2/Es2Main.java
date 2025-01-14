f963af9c9c7ff6bdd3e919e54b94a14f239ad232
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-01-14 16:51:07 +0100

    TUTTO L'ESERCIZIO 2

--- src/Es2/Es2Main.java
+++ src/Es2/Es2Main.java
@@ -4,5 +4,9 @@ public class Es2Main {
     public static  void main (String[] args){
         NumeriCasuali generatoreRandom = new NumeriCasuali();
         generatoreRandom.nCasuali();
+        System.out.println("A capo mostriamo la lista di numeri, ma invertita");
+        generatoreRandom.nuovaLista();
+        System.out.println("Ed ora scegli se stampare i numeri pari o dispari");
+        generatoreRandom.stampaPariDispari();
     }
 }
--- src/Es2/NumeriCasuali.java
+++ src/Es2/NumeriCasuali.java
@@ -1,32 +1,39 @@
 package Es2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class NumeriCasuali {
     private int numeroScelto;
-    private List <Integer> listaCasuali;
+    private List<Integer> listaCasuali;
 
-    public NumeriCasuali(){
-        this.listaCasuali= new ArrayList<Integer>();
+    public NumeriCasuali() {
+        this.listaCasuali = new ArrayList<Integer>();
     }
 
-    void nCasuali (){
+    void nCasuali() {
         System.out.println("Inserisci quanti numeri casuali vuoi che vengano generati.");
         Scanner myScanner = new Scanner(System.in);
         numeroScelto = myScanner.nextInt();
         myScanner.nextLine();
-        for (int i =0; i< numeroScelto; i++){
+        for (int i = 0; i < numeroScelto; i++) {
             int nScelto = (int) (Math.random() * 101);
             listaCasuali.add(nScelto);
             System.out.println(nScelto);
         }
     }
 
-    void nuovaLista (List <Integer> n){
-        n = listaCasuali;
+    void nuovaLista() {
+        Collections.reverse(listaCasuali);
+        System.out.println(listaCasuali);
     }
 
+    void stampaPariDispari() {
+        Scanner myScanner = new Scanner(System.in);
+        boolean c = myScanner.nextBoolean();
+        for (int i = 0; i < listaCasuali.size(); i++) {
+            if (c && i % 2 == 0) System.out.println("Indice pari, " + listaCasuali.get(i));
+            else if (c) System.out.println("Indice dispari, " + listaCasuali.get(i));
+        }
 
+    }
 }
